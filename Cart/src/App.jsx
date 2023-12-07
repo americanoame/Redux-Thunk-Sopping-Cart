c462c35f658ed22bbe7d7fe43d25f8dd6b3e7780
Americano.Dev <andrenations@gmail.com>
2023-12-07 03:34:36 -0500

    created a cart container and clearCart functionality

--- Cart/src/App.jsx
+++ Cart/src/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from './components/Navbar';
+import CartContainer from './components/CartContainer';
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <main>
       <Navbar />
+      <CartContainer />
     </main>
   )
 }
