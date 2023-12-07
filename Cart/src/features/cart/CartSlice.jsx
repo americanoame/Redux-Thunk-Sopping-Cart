ea8c349ef9598fe919577451c388ecf3a0b581f5
Americano.Dev <andrenations@gmail.com>
2023-12-07 03:42:04 -0500

    added functionality remove, increase and decrease on it

--- Cart/src/features/cart/CartSlice.jsx
+++ Cart/src/features/cart/CartSlice.jsx
@@ -1,20 +1,51 @@
-import  { createSlice} from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 import CartItems from '../../CartItems';
 
+// immer library we don't have to return a new state and always avoid mutation
+// because basically with user useReducer we always need to return the new state
+// however with immer library behind the scenes does all the heavy lifting so in this case
+// we can modify the state or mutate the state directly. (clearCart) will be the
+// the code that mutates the state
 
 const initialState = {
-    cartItems: CartItems, 
-    amount: 0 , 
-    total: 0,
-    isLoading: true,
-}
+  cartItems: CartItems,
+  amount: 4,
+  total: 0,
+  isLoading: true,
+};
 
 const cartSlice = createSlice({
-    name: 'cart',
-    initialState,
+  name: 'cart',
+  initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
 
+    removeItem: (state, action) => {
+        const itemId = action.payload;
+        state.cartItems = state.cartItems.filter((item) => 
+         item.id !== itemId);
+    },
+
+    // functionality is, we look for the payload which is an object thats why we go
+        // with payload.id to show case that we can pass more data and then we just increase the amount
+
+        increase: (state, {payload}) => {
+            const cartItem = state.cartItems.find((item) => item.id
+            === payload.id);
+            cartItem.amount = cartItem.amount + 1;
+        },
+
+        decrease: (state, {payload}) => {
+            const cartItem = state.cartItems.find((item) => item.id
+            === payload.id);
+            cartItem.amount = cartItem.amount - 1;
+        },
+  },
 });
 
 // console.log(cartSlice);
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
 
-export default cartSlice.reducer
+export default cartSlice.reducer;
